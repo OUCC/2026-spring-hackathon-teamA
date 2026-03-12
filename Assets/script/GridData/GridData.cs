cba64572db71d844ee5f2749b35a27fc41e7e0c7
kouyou <98743479+git-kouyou@users.noreply.github.com>
2026-03-12 21:12:00 +0900

    クラスのファイルを追加

---
+++ Assets/script/GridData/GridData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridData
+{
+    [SerializeField] private Tilemap tilemap;
+    GridData(int width, int height)
+    {
+        
+    }
+}
